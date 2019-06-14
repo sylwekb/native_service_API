5b50bba6c9e705f57047c46c6c6b6b2a20496318
codemangasior <lukasz.gasiorowski92@gmail.com>
2019-06-14 15:51:31 +0200

    new model for performer pricing

--- Native_Service/admin.py
+++ Native_Service/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import NativePost
+from .models import NativePost, FinalPricing
 
 admin.register(admin)
 admin.site.register(NativePost)
+admin.site.register(FinalPricing)
+
--- Native_Service/models.py
+++ Native_Service/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class NativePost(models.Model):
@@ -16,7 +17,14 @@ class NativePost(models.Model):
     def __str__(self):
         return self.title
 
+
 class FinalPricing(models.Model):
     time_to_get_ready = models.DateField()
     price = models.CharField(max_length=10)
     comments = models.CharField(max_length=500)
+    secret_key = models.CharField(max_length=12)
+
+    """
+    def get_absolute_url(self):
+        return reverse("finalpricing", kwargs={"secret_key": self.secret_key})
+    """
